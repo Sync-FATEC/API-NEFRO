a9a2a5295b41d28fcb489328597bff1d5d43ff2e
Arthur Custódio Alkmin <arthurcustodioalkmin@gmail.com>
2023-10-03 08:41:24 -0300

    Adicionei o template base das paginas html

--- src/views.py
+++ src/views.py
@@ -3,4 +3,20 @@ from flask import render_template, request, redirect, url_for, session, flash
 
 @app.route('/')
 def index():
-    return 'Bem-vindo'
+    return render_template('home.html')
+
+@app.route('/drcBrasil')
+def drcBrasil():
+    return render_template('drcBrasil.html')
+
+@app.route('/proadi')
+def proadi():
+    return render_template('proadi.html')
+
+@app.route('/comunidade')
+def comunidade():
+    return render_template('comunidade.html')
+
+@app.route('/perguntasFrequentes')
+def perguntasFrequentes():
+    return render_template('perguntasFrequentes.html')
