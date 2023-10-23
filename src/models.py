8ee16c494add5ae6d9bfc203a8af1bcf8e1cac4e
henrique godoy <henriacolino@gmail.com>
2023-10-23 13:11:39 -0300

    criação do banco de dados para cadastro

--- src/models.py
+++ src/models.py
@@ -1,13 +1,13 @@
 from app import db
 
-class Cadastro(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String, nullable=False)
-    data_nasc = db.Column(db.DateTime, nullable=False)
-    endereco = db.Column(db.String(255), nullable=False)
-    cpf = db.Column(db.String(11), nullable=False)
-    email = db.Column(db.String, nullable=False)
-    senha = db.Column(db.String, nullable=False)
-    parentesco = db.Column(db.String(20), nullable=False)
-    profissao = db.Column(db.String(50), nullable=False)
-    como_chegou = db.Column(db.String(50), nullable=False)
+class Usuario(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_nome = db.Column(db.String, nullable=False)
+    user_data_nasc = db.Column(db.DateTime, nullable=False)
+    user_endereco = db.Column(db.String(255), nullable=False)
+    user_cpf = db.Column(db.String(11), nullable=False)
+    user_email = db.Column(db.String, nullable=False)
+    user_senha = db.Column(db.String, nullable=False)
+    user_user_parentesco = db.Column(db.String(20), nullable=False)
+    user_profissao = db.Column(db.String(50), nullable=False)
+    user_como_chegou = db.Column(db.String(50), nullable=False)
