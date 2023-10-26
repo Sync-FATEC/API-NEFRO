353ab3d98ecf6144d8bf7e333ac110af974161f3
Filipe Colla <filipe10colla@gmail.com>
2023-10-26 12:25:31 -0300

    Arrumei o redirecionamento pós autenticação do login e cadastro

--- src/routes/views.py
+++ src/routes/views.py
@@ -2,43 +2,51 @@ from app import app, db
 from flask import render_template, request, redirect, url_for, session, flash
 from models import *
 
+def user():
+    if 'user' not in session or session['user'] == None:
+        user = None
+
+    else:
+        user = Usuario.query.filter_by(user_id=session['user']).first()
+    return user
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', user = user())
 
 @app.route('/drcBrasil')
 def drcBrasil():
-    return render_template('drcBrasil.html')
+    return render_template('drcBrasil.html', user = user())
 
 @app.route('/comunidade')
 def comunidade():
-    return render_template('comunidade.html')
+    return render_template('comunidade.html', user = user())
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    return render_template('login.html', user = user())
 
 # Cadastro de usuarios;
 @app.route('/cadastro')
 def cadastro():
-    return render_template('cadastro.html')
+    return render_template('cadastro.html', user = user())
 
 @app.route('/blog-logado')
 def blog():
-    return render_template('blog-logado.html')
+    return render_template('blog-logado.html', user = user())
 
 
 @app.route('/historia-logado')
 def hist():
-    return render_template('historia-logado.html')
+    return render_template('historia-logado.html', user = user())
 
 @app.route('/quemsomos')
 def quemsomos():
-    return render_template('quemsomos.html')
+    return render_template('quemsomos.html', user = user())
 
 @app.route('/historia')
 def historia():
-    return render_template('historia.html')
+    return render_template('historia.html', user = user())
 
 @app.route('/perguntasFrequentes')
 def perguntasFrequentes():
@@ -50,4 +58,4 @@ def perguntasFrequentes():
         for i in range(0, len(texto), 2):
             conteudoFaq.append([texto[i], texto[i+1]])
    
-    return render_template('perguntasFrequentes.html', conteudoFaq=conteudoFaq)
+    return render_template('perguntasFrequentes.html', user = user(), conteudoFaq=conteudoFaq)
