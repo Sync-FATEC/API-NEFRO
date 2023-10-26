0fe1dbe8e265d78c6eccd66c6e1afa670764f907
Filipe Colla <filipe10colla@gmail.com>
2023-10-26 12:23:58 -0300

    Fiz a autenticação do login e logout, além de arrumar o cadastro

--- src/routes/autenticar.py
+++ src/routes/autenticar.py
@@ -35,9 +35,37 @@ def cadastrar():
         novoUsuario = Usuario (user_nome=nomeCompleto, user_data_nasc=dataNascimento, user_cpf=cpf, user_endereco=endereco, user_email=email, user_parentesco=parentesco, user_profissao=profissao, user_como_chegou=comoChegou, user_senha=senha)
         db.session.add(novoUsuario)
         db.session.commit()
+
+        session["user"] = novoUsuario.user_id
+
     else:
          flash('As senhas devem ser identicas!')
 
 
     # Redirecionando para homepage;     
     return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session['user'] = None
+
+    return redirect(url_for('index'))
+
+@app.route('/logar', methods=['POST'])
+def logar():
+    email = request.form['email']
+    senha = request.form['senha']
+
+    if not Usuario.query.filter_by(user_email=email).first():
+        flash('Email inexistente')
+        print(1)
+        return redirect(url_for('login'))
+    elif Usuario.query.filter_by(user_email=email).first().user_senha != senha:
+        flash('Senha incorreta')
+        print(2)
+        return redirect(url_for('login'))
+    else:
+        session['user'] = Usuario.query.filter_by(user_email=email).first().user_id
+        print(3)
+        return redirect(url_for('index'))
+    
