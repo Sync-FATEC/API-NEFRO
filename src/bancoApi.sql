3ee3f8d14d8c394d3d6fdaa5196dea1987135970
Manhã <joaoggbs62@gmail.com>
2023-10-30 08:50:26 -0300

    Alteração no banco de dados

---
+++ src/bancoApi.sql
@@ -0,0 +1,34 @@
+drop database if exists Api;
+
+create database Api;
+
+use Api;
+
+CREATE TABLE Usuario (
+
+user_id int primary key  auto_increment,
+user_nome varchar (100) not null,
+user_data_nasc date not null,
+user_endereco varchar (100) not null,
+user_cpf varchar (11) not null unique,
+user_email varchar (100) not null unique,
+user_senha varchar (26) not null,
+user_parentesco varchar (20) not null,
+user_profissao varchar (50) not null,
+user_como_chegou varchar (50) not null
+);
+
+
+CREATE TABLE Comentarios (
+com_id INT AUTO_INCREMENT PRIMARY KEY,
+com_historia LONGTEXT NOT NULL,
+com_data DATE,
+com_imagem varchar (255),
+com_nome_filho varchar (255),
+com_aprovado BOOLEAN default false,
+com_denuncia BOOLEAN default false,
+fk_user_id INT NOT NULL,
+fk_com_id INT,
+FOREIGN KEY (fk_user_id) REFERENCES Usuario(user_id),
+FOREIGN KEY (fk_com_id) REFERENCES Comentarios(com_id)
+);
