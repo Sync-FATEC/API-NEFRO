9a3680effe7ecb5b1210c18b4fb4cc613d7c0b0e
Manhã <joaoggbs62@gmail.com>
2023-11-01 08:15:28 -0300

    Revisão das regras de negócio do banco de dados

--- src/bancoApi.sql
+++ src/bancoApi.sql
@@ -9,16 +9,18 @@ CREATE TABLE usuario (
 user_id int primary key  auto_increment,
 user_nome varchar (100) not null,
 user_data_nasc date not null,
-user_endereco varchar (100) not null,
+user_endereco varchar (100) default Null,
 user_cpf varchar (11) not null unique,
 user_email varchar (100) not null unique,
 user_senha varchar (26) not null,
-user_parentesco varchar (20) not null,
-user_profissao varchar (50) not null,
-user_como_chegou varchar (50) not null,
+user_parentesco varchar (20) default Null,
+user_profissao varchar (50) default Null,
+user_como_chegou varchar (50) default Null,
 user_admin BOOLEAN default false
 );
 
+INSERT INTO usuario(user_nome, user_data_nasc, user_cpf, user_email, user_senha, user_admin) 
+VALUES('admin', '2023-11-01', '00000000000', 'admin@admin.com', 'adminadmin', '1');
 
 CREATE TABLE comentarios (
 com_id INT AUTO_INCREMENT PRIMARY KEY,
